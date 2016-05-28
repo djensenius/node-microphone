96ed47d39dd8094d9c518b36508eeae6ad0c96a5
David Jensenius <djensenius@gmail.com>
2016-05-28 07:54:52 -0400

    Change default input

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ var info = new PassThrough;
 
 var start = function(options) {
     options = options || {};
-    
+
     if(ps == null) {
         ps = isMacOrWin
         ? spawn('sox', ['-d', '-t', 'dat', '-p'])
-        : spawn('arecord', ['-D', 'plughw:1,0', '-f', 'dat']);
+        : spawn('arecord', ['-D', 'plughw:0,0', '-f', 'dat']);
 
         if(options.mp3output === true) {
             var encoder = new lame.Encoder( {
